10d98a0b86a3b5185a5dd8dc7c85e808b9672b58
pjmorante <pedro.morante@outlook.com>
2022-01-18 19:55:12 -0500

    feat: add footer-component

--- packages/react-food-ya/src/components/Footer/index.js
+++ packages/react-food-ya/src/components/Footer/index.js
@@ -1,13 +1,52 @@
-import { React, Component } from 'react';
+import { React } from "react";
+import { IconContext } from "react-icons/lib";
+import { BsFacebook, BsTwitter, BsLinkedin, BsYoutube } from "react-icons/bs";
 
-class Footer extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Footer</h3>
+const Footer = () => {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          {/* col-1 */}
+          <div className="col-1">
+            <img src="https://i.ibb.co/DDsSFVZ/logo.png" alt="logo" />
+          </div>
+          {/* col-2 */}
+          <IconContext.Provider value={{ color: "#f4ece1", size: "24px" }}>
+            <div className="col-2">
+              <ul className="list-unstyled">
+                <li>
+                  <a href="https://www.facebook.com/">
+                    <BsFacebook />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/?lang=es">
+                    <BsTwitter />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://co.linkedin.com/">
+                    <BsLinkedin />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/?hl=ES">
+                    <BsYoutube />
+                  </a>
+                </li>
+              </ul>
             </div>
-        );
-    }
-}
+          </IconContext.Provider>
+          {/* col-3 */}
+          <div className="col">
+            <p className="contact">example@gmail.com</p>
+            <p className="contact">#555555555555</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;

6a2afd06300e306cff98d2c01afa67d3df6863c3
Aldhair Vera Camacho <aldhairvera15@gmail.com>
2022-01-20 14:11:50 -0500

    feat: add react router

--- packages/react-food-ya/src/App.js
+++ packages/react-food-ya/src/App.js
@@ -1,9 +1,29 @@
 import Landing from './pages/Landing/';
+import RegisterSelection from './pages/RegisterSelection/';
+import Footer from './components/Footer/';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Register from './pages/Register/';
+import SignIn from './pages/SignIn/';
+
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Landing/>}/>
+          <Route path="/register-selection" element={<RegisterSelection/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+        </Routes>
+      <Footer/>
+      </BrowserRouter>
     </div>
   );
 }

ad1b9d3b23c9240f0794949cffc02580404552a3
Fernando Rodriguez <frodriguezv06@gmail.com>
2022-02-06 22:23:44 -0500

    feat: add RegisterRestaurant page
    add FormRestaurant component

---
+++ packages/react-food-ya/src/components/FormRestaurant/index.jsx
@@ -0,0 +1,106 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+// import React, { useState } from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Form, Row, Col, Button,
+} from 'react-bootstrap';
+
+// import CustomButton from '../CustomButton';
+
+function FormRestaurant() {
+  const [restaurantState, setRestaurant] = useState({});
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRestaurant({ ...restaurantState, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (restaurantState.password !== restaurantState.password_configuration) {
+      navigate('/register-selection');
+    } else {
+      navigate('/');
+    }
+  };
+
+  return (
+    <Form className="form-restaurant" onSubmit={handleSubmit}>
+      <h1>Registrar</h1>
+      <h2>Datos Personales</h2>
+      <Form.Group as={Row} controlId="formFile" className="mb-3">
+        <Col sm="6" className="mb-3">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control type="text" name="name" onChange={handleChange} />
+        </Col>
+
+        <Col sm="6" className="mb-3">
+          <Form.Label>Apellido</Form.Label>
+          <Form.Control type="text" name="surname" onChange={handleChange} />
+        </Col>
+
+        <Col sm="12" className="mb-3">
+          <Form.Label>Correo</Form.Label>
+          <Form.Control type="text" name="email" onChange={handleChange} />
+        </Col>
+
+        <Col sm="6" className="mb-3">
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control type="text" name="password" onChange={handleChange} />
+        </Col>
+
+        <Col sm="6" className="mb-3">
+          <Form.Label>Confirmar Contraseña</Form.Label>
+          <Form.Control type="text" name="password_configuration" onChange={handleChange} />
+        </Col>
+
+        <Col sm="12" className="mb-3">
+          <Form.Label>DNI</Form.Label>
+          <Form.Control type="text" name="dni" onChange={handleChange} />
+        </Col>
+
+        <Col sm="12" className="mb-3">
+          <Form.Label>Dirección</Form.Label>
+          <Form.Control type="text" name="direction" onChange={handleChange} />
+        </Col>
+
+        <Col sm="6" className="mb-3">
+          <Form.Label>Distrito</Form.Label>
+          <Form.Control type="text" name="district" onChange={handleChange} />
+        </Col>
+
+        <Col sm="6" className="mb-3">
+          <Form.Label>Ciudad</Form.Label>
+          <Form.Control type="text" name="city" onChange={handleChange} />
+        </Col>
+        <h2>Datos del Restaurant</h2>
+        <Col sm="12" className="mb-3">
+          <Form.Label>Razón Social</Form.Label>
+          <Form.Control type="text" name="business_name" onChange={handleChange} />
+        </Col>
+
+        <Col sm="6" className="mb-3">
+          <Form.Label>RUC</Form.Label>
+          <Form.Control type="text" name="ruc" onChange={handleChange} />
+        </Col>
+        <Col sm="6" className="mb-3">
+          <Form.Label>Teléfono del local</Form.Label>
+          <Form.Control type="text" name="phone_number" onChange={handleChange} />
+        </Col>
+        <Col sm="12" className="mb-3">
+          <Form.Label>Correo del local</Form.Label>
+          <Form.Control type="text" name="business_email" onChange={handleChange} />
+        </Col>
+        <Col sm="12" className="mb-3">
+          <Form.Label>Dirección del local</Form.Label>
+          <Form.Control type="text" name="business_address" onChange={handleChange} />
+        </Col>
+        <Button type="submit" className="Reg-button">Registar</Button>
+      </Form.Group>
+    </Form>
+  );
+}
+
+export default FormRestaurant;
---
+++ packages/react-food-ya/src/pages/RegisterRestaurant/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+import FormRestaurant from '../../components/FormRestaurant';
+
+function RegisterRestaurant() {
+  return (
+    <div className="register-restaurant">
+      <FormRestaurant />
+    </div>
+  );
+}
+
+export default RegisterRestaurant;

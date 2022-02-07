02effe893eab35bf48d8b554ebab47c7bbf19117
Fernando Rodriguez <frodriguezv06@gmail.com>
2022-02-06 22:36:00 -0500

    fix: add navbar-collapse

--- packages/react-food-ya/src/components/Navbar/index.jsx
+++ packages/react-food-ya/src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Container, Navbar, NavDropdown, Nav,
+  Container, Navbar, Nav,
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './style.scss';
@@ -28,8 +28,8 @@ function CustomNavbar() {
     //     </li>
     //   </ul>
     // </nav>
-    <Navbar expand="lg">
-      <Container>
+    <Navbar expand="md">
+      <Container fluid>
         <Navbar.Brand>
           <Link className="logo-link" to="/">
             <img
@@ -41,16 +41,9 @@ function CustomNavbar() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ms-auto align-items-end">
+            <Nav.Link href="/sign-in" className="ButtonHeader">Sign In</Nav.Link>
+            <Nav.Link href="/register-selection" className="ButtonHeader">Sign Up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>

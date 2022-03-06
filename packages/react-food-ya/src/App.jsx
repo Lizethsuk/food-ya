29b257c9cf7377140a4af4dd7c9e2f874aea0879
Aldhair Vera Camacho <aldhairvera15@gmail.com>
2022-03-06 05:29:44 -0500

    feat: routes componentized

--- packages/react-food-ya/src/App.jsx
+++ packages/react-food-ya/src/App.jsx
@@ -2,61 +2,21 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 /* eslint-disable import/no-cycle */
 import React, { useContext, useEffect } from 'react';
-import {
-  BrowserRouter, Routes, Route, Navigate,
-} from 'react-router-dom';
-import Home from './pages/Home';
-import Landing from './pages/Landing';
-import RegisterSelection from './pages/RegisterSelection';
-import SignInSelection from './pages/SignInSelection';
-import Footer from './components/Footer';
-import CustomNavbar from './components/CustomNavbar';
-import RegisterUser from './pages/RegisterUser';
-import ConfirmationRegister from './pages/ConfirmationRegister';
-import RegisterRestaurant from './pages/RegisterRestaurant';
-import SignInUser from './pages/SignInUser';
-import SignInRestaurant from './pages/SignInRestaurant';
-import RestaurantPage from './pages/RestaurantPage';
-import DishesManager from './pages/DishesManager';
-import NotFound from './pages/NotFound';
+import AppRoutes from './routes/AppRoutes';
 import { ThemeContext } from './context/themeContext';
 import { UserContext } from './context/userContext';
 
 function App() {
   const { theme } = useContext(ThemeContext);
-  const { Initialize, user } = useContext(UserContext);
+  const { Initialize } = useContext(UserContext);
 
   useEffect(() => {
     Initialize();
   }, []);
 
-  const condition = user.token !== '';
-
   return (
     <div className={`App ${theme}`}>
-      <BrowserRouter>
-        <CustomNavbar />
-        <Routes>
-          <Route path="/" element={(condition ? <Navigate to="/home" /> : <Landing />)} />
-          {/* <Route path="/register-selection" element={<RegisterSelection />} /> */}
-          <Route
-            path="/register-selection"
-            element={(condition ? <Navigate to="/home" /> : <RegisterSelection />)}
-          />
-          <Route path="/register-user" element={(condition ? <Navigate to="/home" /> : <RegisterUser />)} />
-          <Route path="/register-restaurant" element={(condition ? <Navigate to="/home" /> : <RegisterRestaurant />)} />
-          <Route path="/restaurant" element={<RestaurantPage />} />
-          <Route path="/confirmation-register" element={(condition ? <Navigate to="/home" /> : <ConfirmationRegister />)} />
-          <Route path="/sign-in-selection" element={(condition ? <Navigate to="/home" /> : <SignInSelection />)} />
-          <Route path="/sign-in-user" element={(condition ? <Navigate to="/home" /> : <SignInUser />)} />
-          <Route path="/sign-in-restaurant" element={(condition ? <Navigate to="/home" /> : <SignInRestaurant />)} />
-          <Route path="/home" element={<Home />} />
-
-          <Route path="/dish-manager/:restaurantId" element={<DishesManager />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <AppRoutes />
     </div>
   );
 }
---
+++ packages/react-food-ya/src/routes/AppRoutes.jsx
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import {
+  BrowserRouter, Routes, Route, Navigate,
+} from 'react-router-dom';
+import Home from '../pages/Home';
+import Landing from '../pages/Landing';
+import RegisterSelection from '../pages/RegisterSelection';
+import SignInSelection from '../pages/SignInSelection';
+import Footer from '../components/Footer';
+import CustomNavbar from '../components/CustomNavbar';
+import RegisterUser from '../pages/RegisterUser';
+import ConfirmationRegister from '../pages/ConfirmationRegister';
+import RegisterRestaurant from '../pages/RegisterRestaurant';
+import SignInUser from '../pages/SignInUser';
+import SignInRestaurant from '../pages/SignInRestaurant';
+import RestaurantPage from '../pages/RestaurantPage';
+import DishesManager from '../pages/DishesManager';
+import NotFound from '../pages/NotFound';
+import { UserContext } from '../context/userContext';
+
+function AppRoutes() {
+  const { user } = useContext(UserContext);
+  const condition = user.token !== '';
+
+  return (
+    <BrowserRouter>
+      <CustomNavbar />
+      <Routes>
+        <Route path="/" element={(condition ? <Navigate to="/home" /> : <Landing />)} />
+        {/* <Route path="/register-selection" element={<RegisterSelection />} /> */}
+        <Route
+          path="/register-selection"
+          element={(condition ? <Navigate to="/home" /> : <RegisterSelection />)}
+        />
+        <Route path="/register-user" element={(condition ? <Navigate to="/home" /> : <RegisterUser />)} />
+        <Route path="/register-restaurant" element={(condition ? <Navigate to="/home" /> : <RegisterRestaurant />)} />
+        <Route path="/restaurant" element={<RestaurantPage />} />
+        <Route path="/confirmation-register" element={(condition ? <Navigate to="/home" /> : <ConfirmationRegister />)} />
+        <Route path="/sign-in-selection" element={(condition ? <Navigate to="/home" /> : <SignInSelection />)} />
+        <Route path="/sign-in-user" element={(condition ? <Navigate to="/home" /> : <SignInUser />)} />
+        <Route path="/sign-in-restaurant" element={(condition ? <Navigate to="/home" /> : <SignInRestaurant />)} />
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/dish-manager/:restaurantId" element={<DishesManager />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;

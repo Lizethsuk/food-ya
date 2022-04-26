dec3d14f7658264a3c1108f4ead757f75bd002b9
Aldhair Vera <aldhairvera15@gmail.com>
2022-04-26 16:09:34 -0500

    fix: sendgrid active

--- packages/express-food-ya/config/sendgrid.js
+++ packages/express-food-ya/config/sendgrid.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const sgMail = require('@sendgrid/mail')
 const foodyaEmail = process.env.SENDGRID_EMAIL
-const isDev = true
+const isDev = false
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-module.exports = {sgMail, foodyaEmail, isDev}
+module.exports = { sgMail, foodyaEmail, isDev }
